abc99eba8abbec4ead9e9df6ba9b6c6bd651ecae
ukiuni <ukiuni@gmail.com>
2015-11-16 21:24:11 +0900

    Show not https content.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var app = require('app'); // Module to control application life.
 var BrowserWindow = require('browser-window'); // Module to create native
-												// browser window.
+// browser window.
 // Report crashes to our server.
 require('crash-reporter').start();
 // Keep a global reference of the window object, if you don't, the window will
@@ -10,11 +10,10 @@ var mainWindow = null;
 app.on('window-all-closed', function() {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform != 'darwin') {
+	if (true || process.platform != 'darwin') {// ALL Quit
 		app.quit();
 	}
 });
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
@@ -22,10 +21,14 @@ app.on('ready', function() {
 	mainWindow = new BrowserWindow({
 		width : 800,
 		height : 600,
-		transient : true
+		"node-integration" : false,
+		"web-preferences" : {
+			"web-security" : false
+		}
 	});
 	// and load the index.html of the app.
-	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	// mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	mainWindow.loadUrl('https://slite.ukiuni.com');
 	// Open the DevTools.
 	mainWindow.webContents.openDevTools();
 	// Emitted when the window is closed.
