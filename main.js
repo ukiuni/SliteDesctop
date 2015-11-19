e5abfaea4a666c8a3c7e4098f29d53e18ea8180c
ukiuni <ukiuni@gmail.com>
2015-11-19 12:51:02 +0900

    Copy & Paste enabled on Mac.

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ app.on('ready', function() {
 	// mainWindow.loadUrl('file://' + __dirname + '/index.html');
 	mainWindow.loadUrl(process.env.TARGET || 'https://slite.ukiuni.com');
 	// Open the DevTools.
-	if(process.env.DEV){
+	if (process.env.DEV) {
 		mainWindow.webContents.openDevTools();
 	}
 	// Emitted when the window is closed.
@@ -39,4 +39,50 @@ app.on('ready', function() {
 		// when you should delete the corresponding element.
 		mainWindow = null;
 	});
+	var Menu = require("menu");
+	var template = [ {
+		label : "Application",
+		submenu : [ {
+			label : "About Application",
+			selector : "orderFrontStandardAboutPanel:"
+		}, {
+			type : "separator"
+		}, {
+			label : "Quit",
+			accelerator : "Command+Q",
+			click : function() {
+				app.quit();
+			}
+		} ]
+	}, {
+		label : "Edit",
+		submenu : [ {
+			label : "Undo",
+			accelerator : "CmdOrCtrl+Z",
+			selector : "undo:"
+		}, {
+			label : "Redo",
+			accelerator : "Shift+CmdOrCtrl+Z",
+			selector : "redo:"
+		}, {
+			type : "separator"
+		}, {
+			label : "Cut",
+			accelerator : "CmdOrCtrl+X",
+			selector : "cut:"
+		}, {
+			label : "Copy",
+			accelerator : "CmdOrCtrl+C",
+			selector : "copy:"
+		}, {
+			label : "Paste",
+			accelerator : "CmdOrCtrl+V",
+			selector : "paste:"
+		}, {
+			label : "Select All",
+			accelerator : "CmdOrCtrl+A",
+			selector : "selectAll:"
+		} ]
+	} ];
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
