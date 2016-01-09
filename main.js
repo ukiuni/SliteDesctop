082fb052f30c628669a4723973d9c4b799c96f0d
ukiuni <ukiuni@gmail.com>
2016-01-10 08:59:27 +0900

    Reload on server updated.

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
-var app = require('app'); // Module to control application life.
-var BrowserWindow = require('browser-window'); // Module to create native
-// browser window.
-// Report crashes to our server.
+var app = require('app');
+var ipc = require('ipc');
+var BrowserWindow = require('browser-window');
 require('crash-reporter').start();
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
 	if (true || process.platform != 'darwin') {// ALL Quit
 		app.quit();
 	}
 });
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-	// Create the browser window.
 	mainWindow = new BrowserWindow({
 		width : 800,
 		height : 600,
@@ -25,9 +16,11 @@ app.on('ready', function() {
 			"web-security" : false
 		}
 	});
-	// and load the index.html of the app.
 	// mainWindow.loadUrl('file://' + __dirname + '/index.html');
 	mainWindow.loadUrl(process.env.TARGET || 'https://slite.ukiuni.com');
+	ipc.on("reload", function(event, arg) {
+		mainWindow.loadUrl(arg);
+	});
 	// Open the DevTools.
 	if (process.env.DEV) {
 		mainWindow.webContents.openDevTools();
